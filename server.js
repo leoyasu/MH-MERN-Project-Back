d187ca5b62ff1b1375c54658b3877beed1464832
LeandroYasutake <leandroyasutake@gmail.com>
2023-10-07 12:04:15 -0300

    added cors

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 require("dotenv").config();
+const cors = require("cors");
 require("./config/database.js")
 
 const doctorRouter = require('./routes/doctorRouter')
 const app = express();
-const PORT = process.env.PORT || 5000;
-app.use(express.json())
 
-app.set("port",PORT);
-app.get("/",(req,res)=> {
+app.use(express.json())
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true,
+    })
+);
+
+app.get("/", (req, res) => {
     res.send("servidor creado")
 });
 
-app.use('/api',doctorRouter)
-
-app.listen(PORT,()=>console.log("servidor corriendo en el puerto: "+PORT));
+app.use('/api', doctorRouter)
+const PORT = process.env.PORT || 4000;
+app.set("port", PORT);
+app.listen(PORT, () => console.log("servidor corriendo en el puerto: " + PORT));
